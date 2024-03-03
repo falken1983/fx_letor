901acb8bb52605dc1037017c72782e0e24f7b59c
Girbés <lluis2001@protonmail.ch>
2024-03-03 13:29:48 +0100

    ADD: `fx_catcher.py now supports overriden artifacts for unhandled (poweroff) raspi4 events

--- mysql_server_cnf/fx_catcher.py
+++ mysql_server_cnf/fx_catcher.py
@@ -24,58 +24,76 @@ def config():
         return {"codes": fx_codes, 
                 "ticker": ["EUR"+code+"=X" for code in fx_codes]}
     
-print(config()["codes"])
+# print(config()["codes"])
 # *Extract* from Y! EOD Data (23 GMT+1 = 17 EST)
-quotes = yf.download(config()["ticker"], period="1d")["Adj Close"]
 
-# *Transform*: Needed transforms for Inserting in db
-# recoding
-quotes = 1/quotes
-quotes.columns = [
-    iso_code.replace("EUR","").replace("=X","") for iso_code in quotes.columns
-]
-# changing index type (datetime -> str) and label name
-quotes.index = quotes.index.strftime("%Y-%m-%d")
-quotes.index.rename("date", inplace=True)
-# melting (tidy format)
-quotes_tidy = quotes.melt(            
-        var_name="code",
-        value_name="price",
-        ignore_index=False
-).reset_index().sort_values(by=["date","code"])    
+def data_down(override: bool=False, **kwargs: dict) -> None:
+    
+    if override:
+        quotes = yf.download(config()["ticker"], **kwargs)["Adj Close"]
+    else:
+        quotes = yf.download(config()["ticker"], period="1d")["Adj Close"]
 
-with open(PYSCRIPT_PATH + "/secrets.json","r") as f:
-    data_json = json.load(f)   
+    # *Transform*: Needed transforms for Inserting in db
+    # recoding
+    quotes = 1/quotes
+    quotes.columns = [
+        iso_code.replace("EUR","").replace("=X","") for iso_code in quotes.columns
+    ]
+    # changing index type (datetime -> str) and label name
+    quotes.index = quotes.index.strftime("%Y-%m-%d")
+    quotes.index.rename("date", inplace=True)
+    # melting (tidy format)
+    quotes_tidy = quotes.melt(            
+            var_name="code",
+            value_name="price",
+            ignore_index=False
+    ).reset_index().sort_values(by=["date","code"])    
 
-secrets = data_json["sql_authentication"]
+    with open(PYSCRIPT_PATH + "/secrets.json","r") as f:
+        data_json = json.load(f)   
 
-try:
-    conn = msql.connect(
-        host='localhost', 
-        database='finance_market_data_db', 
-        user=secrets["username"], 
-        password=secrets["password"]
-    )
-    if conn.is_connected():
-        cursor = conn.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)        
-        #loop through the data frame
-        for i,row in quotes_tidy.iterrows():
-            #here %S means string values 
-            sql = "INSERT INTO fx_prices(date, code, price) VALUES (%s,%s,%s);"
-            cursor.execute(sql, tuple(row))
-            print("Record inserted")
-            # the connection is not auto committed by default, so we must commit to save our changes
-            conn.commit()
-except Error as e:
-    print("Error while connecting to MySQL", e)
+    secrets = data_json["sql_authentication"]
+
+    try:
+        conn = msql.connect(
+            host='localhost', 
+            database='finance_market_data_db', 
+            user=secrets["username"], 
+            password=secrets["password"]
+        )
+        if conn.is_connected():
+            cursor = conn.cursor()
+            cursor.execute("select database();")
+            record = cursor.fetchone()
+            print("You're connected to database: ", record)        
+            #loop through the data frame
+            for i,row in quotes_tidy.iterrows():
+                #here %S means string values 
+                sql = "INSERT INTO fx_prices(date, code, price) VALUES (%s,%s,%s);"
+                cursor.execute(sql, tuple(row))
+                print("Record inserted")
+                # the connection is not auto committed by default, so we must commit to save our changes
+                conn.commit()
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+
+if __name__=="__main__":
+    
+    """OVERRIDERS ZONE
+              overrider_dict = {
+        "start": "2024-03-01",
+        "end": "2024-03-03"
+    }
+
+    data_down(override=True, **overrider_dict) """
+
+    data_down()
 
 #Missing points:
 """
-1. ETL (Extract from Y!, Transform pandas df, Load into SQL Server)
-2. `crontab -e` for schedule daily batch execution on SQL MariaDB Linux Server 
+1. IMPLEMENTED: ETL (Extract from Y!, Transform pandas df, Load into SQL Server)
+2. IMPLEMENTED: `crontab -e` for schedule daily batch execution on SQL MariaDB Linux Server 
 (raspi4 arm64)
-3. Expand YAML or SQL Table to a more extensive Universe of Assets
+3. NOT YET IMPLEMENTED: Expand YAML or SQL Table to a more extensive Universe of Assets
 """
